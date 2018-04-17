0ecedcc76a93a9780a9a5213285c4e2971da645c
kylelevy <kyle-levy@uiowa.edu>
2018-04-17 11:44:08 -0500

    Most of front end working for creating an election. Just need to add ability to select precincts the election is for.

--- js/createElection.js
+++ js/createElection.js
@@ -1,8 +1,21 @@
 $(document).on('click','#addCandidate',function(){
-        var newCandidate = $('#newCandidate').val();
-        var candidateParty = $('#candidateParty').val();
 
-        if(newCandidate.length > 0 && candidateParty.length > 0){
-            $('#candidateList').append('<li class="candidateInfo">' +'<p>' + newCandidate + '</p>'+ ', ' + '<p>' + candidateParty + '</p>'+ '</li>');
-        }
+    var select = '<select name="politicalAffiliation">' +
+        '<option value="Democratic">Democratic Party</option>' +
+    '<option value="Republican">Republican Party</option>'+
+    '<option value="Libertarian">Libertarian Party</option>'+
+    '<option value="Green">Green Party</option>'+
+    '<option value="Constitution">Constitution Party</option>'+
+    '</select>';
+
+
+
+    $('#candidateList').append('<li class="candidateInfo">' +'<input type = "text" name ="candidateName">'+ select  + '<input type = "image" src = "../resources/x.png" class ="deleteCandidate" alt ="Submit" width="25" height = "25" value = "Working" ></li>');
+
     });
+
+$('#candidateList').on('click','.deleteCandidate',function(){
+    var removeLi = this.parentElement;
+    var liParent = removeLi.parentElement;
+    liParent.removeChild(removeLi);
+})
--- webpages/createElection.php
+++ webpages/createElection.php
@@ -11,18 +11,41 @@ session_start();
 <body>
 <div id = "page">
     <div>Create an election</div>
+    <br>
+    <div class="row">
+        <div class="column">Candidate Name</div>
+        <div class="column">Candidate Party</div>
+
+    </div>
     <p id="roleNum"><?= $_SESSION['role']?></p>
-    <ul id = "candidateList">
-    </ul>
+    <form>
+    <ol id = "candidateList">
+        <! Mandatory candidate one>
+        <li><input type = "text" name="candidateName"><select id="politicalParty" name="politicalAffiliation">
+            <option value="Democratic">Democratic Party</option>
+            <option value="Republican">Republican Party</option>
+            <option value="Libertarian">Libertarian Party</option>
+            <option value="Green">Green Party</option>
+            <option value="Constitution">Constitution Party</option>
+        </select></li>
+        <br>
+        <! Mandatory candidate two>
+        <li><input type = "text" name="candidateName"><select id="politicalParty" name="politicalAffiliation">
+            <option value="Democratic">Democratic Party</option>
+            <option value="Republican">Republican Party</option>
+            <option value="Libertarian">Libertarian Party</option>
+            <option value="Green">Green Party</option>
+            <option value="Constitution">Constitution Party</option>
+        </select></li>
 
-    <span>
-    <label for="newCandidate">New Candidate: </label>
-    <input type = "text" id="newCandidate">
-    <label for="candidateParty">Candidate's Party</label>
-    <input type ="text" id="candidateParty"</li>
-    </span>
 
+    </ol>
+    <input type = "submit" value = "Submit Election">
+    </form>
+    <! Add a form submit button>
     <input type ="submit" id="addCandidate" value = "Add Candidate">
+
+
 
 </div>
 <script src="../js/jquery-3.3.1.js"></script>
